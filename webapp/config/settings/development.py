6b69de4c94a12cb9805dc3cbc1a2a155cd6d80d8
shinkeonkim <singun11@kookmin.ac.kr>
2022-07-17 00:49:18 +0900

    feat: postgresql settings

--- webapp/config/settings/development.py
+++ webapp/config/settings/development.py
@@ -2,3 +2,16 @@ from .base import * # noqa pylint: disable=wildcard-import, unused-wildcard-impo
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+DATABASES = (
+    {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env('POSTGRES_DB'),
+            'USER': env('POSTGRES_USER'),
+            'PASSWORD': env('POSTGRES_PASSWORD'),
+            'HOST': 'db',
+            'PORT': env('POSTGRES_PORT'),
+        }
+    }
+)
