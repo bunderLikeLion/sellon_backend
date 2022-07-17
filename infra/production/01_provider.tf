6f1778d0ac77b0abb27a5800c649564c779b63b2
shinkeonkim <singun11@kookmin.ac.kr>
2022-07-17 16:06:23 +0900

    feat: aws region / profile 설정
    배포할 환경에서 sellon profile을 setting해야 함.

---
+++ infra/production/01_provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  profile = "sellon"
+  region = var.region
+}
---
+++ infra/production/02_region.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  type        = string
+  default     = "ap-northeast-2"
+  description = "The AWS Region to create resources in"
+}
