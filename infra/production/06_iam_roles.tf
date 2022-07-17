cb6bad263eab46c5a4511e4cd87b68d5c254ea02
shinkeonkim <singun11@kookmin.ac.kr>
2022-07-17 17:32:27 +0900

    feat: iam_roles settings

---
+++ infra/production/06_iam_roles.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "ecs-host-role" {
+  name               = "ecs_host_role_production"
+  assume_role_policy = file("policies/ecs-role.json")
+}
+
+resource "aws_iam_role_policy" "ecs-instance-role-policy" {
+  name   = "ecs_instance_role_policy"
+  policy = file("policies/ecs-instance-role-policy.json")
+  role   = aws_iam_role.ecs-host-role.id
+}
+
+
+resource "aws_iam_role" "ecs-service-role" {
+  name               = "ecs_service_role_production"
+  assume_role_policy = file("policies/ecs-role.json")
+}
+
+resource "aws_iam_role_policy" "ecs-service-role-policy" {
+  name   = "ecs_service_role_policy"
+  policy = file("policies/ecs-service-role-policy.json")
+  role   = aws_iam_role.ecs-service-role.id
+}
+
+resource "aws_iam_instance_profile" "ecs" {
+  name = "ecs_instance_profile_production"
+  path = "/"
+  role = aws_iam_role.ecs-host-role.name
+}
