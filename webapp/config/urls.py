01306bc23a14d62a59c829e164857cd77c57ea4f
shinkeonkim <singun11@kookmin.ac.kr>
2022-07-18 20:00:40 +0900

    feat: local에서도 staticfile을 nginx가 서빙하도록 함

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include, re_path
-from django.views.static import serve
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -46,7 +44,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
 ]
-
-if settings.DEBUG:
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
