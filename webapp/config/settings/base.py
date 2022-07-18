8c50299f3a0eca50201ecd65196ec477b2050c5d
shinkeonkim <singun11@kookmin.ac.kr>
2022-07-18 20:10:52 +0900

    feat: media settings

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -139,6 +139,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = f'{PROJECT_DIR}/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = f'{PROJECT_DIR}/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
