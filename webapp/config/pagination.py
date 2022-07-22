6be0bf68b4db2fc53886bff5ccbfc0f18d78823c
koomchang <leahpar0401@gmail.com>
2022-07-22 09:09:30 +0900

    fix : pagination pr 반영

--- webapp/config/pagination.py
+++ webapp/config/pagination.py
@@ -1,15 +1,14 @@
-from collections import OrderedDict
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
 class PageNumberPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 10
     # 임시 설정
 
     def get_paginated_response(self, data):
-        return Response(OrderedDict([
-            ('current_page', self.page.number),
-            ('per_page', self.page_size),
-            ('total_pages', self.page.paginator.num_pages),
-        ]))
+        return Response({
+            'current_page': self.page.number,
+            'per_page': self.page_size,
+            'total_pages': self.page.paginator.num_pages,
+        })
