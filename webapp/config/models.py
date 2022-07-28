aaf06159424133b8b0e842f81d8dd10e73224e35
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-28 20:55:56 +0900

    refactor: ModelManger 추가 및 deleted_at 분리 class 작성

--- webapp/config/models.py
+++ webapp/config/models.py
@@ -1,7 +1,28 @@
+from datetime import datetime
+
 from django.db import models
 
 
+class ModelManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+
+class DeletedModelManager(ModelManager):
+    use_for_related_fields = True
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__is_null=True)
+
+
 class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    objects = ModelManager()
+
     created_at = models.DateTimeField(
         verbose_name='추가된 일시',
         auto_now_add=True
@@ -10,6 +31,14 @@ class BaseModel(models.Model):
         verbose_name='수정된 일시',
         auto_now=True
     )
+
+
+class BaseModelWithDeleted(BaseModel):
+    class Meta:
+        abstract = True
+
+    objects = DeletedModelManager()
+
     deleted_at = models.DateTimeField(
         verbose_name='삭제된 일시',
         blank=True,
@@ -17,5 +46,10 @@ class BaseModel(models.Model):
         default=None
     )
 
-    class Meta:
-        abstract = True
+    def delete(self, using=None, keep_parents=False):
+        self.deleted_at = datetime.now()
+        self.save(update_fields=['deleted_at'])
+
+    def restore(self):
+        self.deleted_at = None
+        self.save(update_fields=['deleted_at'])
