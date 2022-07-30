efe5cef7c6bd0805d0bc2224094a0c7cea85d78a
koomchang <leahpar0401@gmail.com>
2022-07-31 02:30:41 +0900

    feat: 다른 User 정보를 조회할 수 있는 API

--- webapp/user/urls.py
+++ webapp/user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from django.views.decorators.http import require_http_methods
 
-from user.views import DestroyUser
-
+from .views import DestroyUser, RetrieveUser
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('dj_rest_auth.registration.urls')),
-    path('user/destroy/', require_http_methods(['DELETE'])(DestroyUser.as_view()), name='destroy'),
+    path('user/destroy/', DestroyUser.as_view(), name='destroy'),
+    path('user/<int:pk>', RetrieveUser.as_view(), name='retrieve'),
 ]
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,10 +1,24 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from .serializers import UserAbstractSerializer
 from .models import User
 
 
 class DestroyUser(generics.DestroyAPIView):
     queryset = User.objects.all()
+
+    def get_object(self):
+        try:
+            instance = self.queryset.get(username=self.request.user.username)
+            return instance
+        except User.DoesNotExist:
+            content = {'No User Exist': 'nothing to see here'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+
+class RetrieveUser(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserAbstractSerializer
 
     def get_object(self):
         try:
