59dd7fd59a23a9fbd73cb385bdbf55a4b07a8f4b
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-31 04:29:00 +0900

    feat: project_group 모델 추가

---
+++ webapp/product/models/project_group.py
@@ -0,0 +1,33 @@
+from auction.models.auction import Auction
+from config import models
+from config.models import SoftDeleteModel
+from user.models import User
+
+
+class ProductGroup(SoftDeleteModel):
+
+    class Meta:
+        db_table = 'product_groups'
+        verbose_name = 'ProductGroup'
+        verbose_name_plural = 'ProductGroups'
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name='유저'
+    )
+    auction = models.ForeignKey(
+        Auction,
+        on_delete=models.SET_NULL,
+        verbose_name='경매'
+    )
+    money = models.IntegerField(
+        verbose_name='추가 금액',
+        null=True,
+        default=0,
+    )
+    description = models.CharField(
+        verbose_name='물품 그룹 설명',
+        max_length=200,
+        blank=True,
+    )
