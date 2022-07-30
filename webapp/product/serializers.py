339927375470148af106c9ef86698990546e4720
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-31 03:04:18 +0900

    feat: ProductCategory ListAPIView 추가

--- webapp/product/serializers.py
+++ webapp/product/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from product.models import Product
+from product.models import Product, ProductCategory
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'quantity', 'quality', 'abstract']
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ['id', 'name']
--- webapp/product/views.py
+++ webapp/product/views.py
@@ -1,8 +1,15 @@
+from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, ProductCategory
+from .serializers import ProductSerializer, ProductCategorySerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductCategoryListAPIView(ListAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+    pagination_class = None
