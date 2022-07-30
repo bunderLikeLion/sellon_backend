11e166286c03d64bf9ba3d611140d2f57b2adcb0
shinkeonkim <singun11@kookmin.ac.kr>
2022-07-31 03:14:00 +0900

    feat: Product API Viewset에서 thumbnail, user 정보를 다루도록 함.

--- webapp/product/serializers.py
+++ webapp/product/serializers.py
@@ -1,9 +1,28 @@
-from rest_framework import serializers
-
 from product.models import Product
+from user.serializers import UserAbstractSerializer
+from file_manager.serializers import ImageSerializer
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(WritableNestedModelSerializer):
+    user = UserAbstractSerializer(read_only=True)
+    thumbnail = ImageSerializer()
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'quantity', 'quality', 'abstract']
+        fields = [
+            'id',
+            'user',
+            'thumbnail',
+            'name',
+            'description',
+            'quantity',
+            'quality',
+            'abstract',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = [
+            'created_at',
+            'updated_at',
+        ]
--- webapp/product/views.py
+++ webapp/product/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from product.permissions import IsProductEditableOrDestroyable
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -6,3 +9,10 @@ from .serializers import ProductSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsProductEditableOrDestroyable,
+    ]
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
