b0f2a6386831f25fb730cf8e0e2f42809e35eebf
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-31 21:34:56 +0900

    feat: auction에 거래 방법 추가

--- webapp/auction/models/auction.py
+++ webapp/auction/models/auction.py
@@ -13,6 +13,14 @@ class Auction(SoftDeleteModel):
         verbose_name = 'Auction'
         verbose_name_plural = 'Auctions'
 
+    DIRECT_DEAL_TYPE = 0
+    DELIVERY_DEAL_TYPE = 1
+
+    DEALING_TYPE = (
+        (DELIVERY_DEAL_TYPE, 'delivery'),
+        (DIRECT_DEAL_TYPE, 'direct'),
+    )
+
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -39,4 +47,11 @@ class Auction(SoftDeleteModel):
     end_at = models.DateTimeField(
         verbose_name='경매 종료 일시',
         null=True,
+    )
+    dealing_type = models.IntegerField(
+        verbose_name='거래 방법',
+        null=False,
+        choices=DEALING_TYPE,
+        default=0,
+        db_index=True,
     )
