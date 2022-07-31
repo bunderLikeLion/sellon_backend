2ba3d98d600b65bb1b16c0546937922e067270bc
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-01 03:25:27 +0900

    fix: product_group_post_delete시 count 감소 되도록 코드 수정

--- webapp/auction/signals/product_group_signal.py
+++ webapp/auction/signals/product_group_signal.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from product.models import ProductGroup
@@ -6,13 +6,11 @@ from product.models import ProductGroup
 
 @receiver(post_save, sender=ProductGroup)
 def product_group_post_save(sender, **kwargs):
-    auction = kwargs['instance'].auction
-    auction.product_groups_count += 1
-    auction.save()
+    product_group = kwargs['instance']
+    auction = product_group.auction
+    if product_group.deleted_at is None:
+        auction.product_groups_count += 1
+    else:
+        auction.product_groups_count -= 1
 
-
-@receiver(post_delete, sender=ProductGroup)
-def product_group_post_delete(sender, **kwargs):
-    auction = kwargs['instance'].auction
-    auction.product_groups_count -= 1
     auction.save()
