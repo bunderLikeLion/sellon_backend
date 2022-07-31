1e7730bbd9775e4c23b8c660eb5d00680394b197
koomchang <leahpar0401@gmail.com>
2022-08-01 03:21:34 +0900

    feat: message 모델 작성

---
+++ webapp/message/models/message.py
@@ -0,0 +1,29 @@
+from django.db import models
+from config.models import BaseModel
+from user.models import User
+
+
+class Message(BaseModel):
+
+    class Meta:
+        db_table = 'messages'
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        verbose_name='받는 사람',
+    )
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        verbose_name='보내는 사람',
+    )
+    content = models.TextField(
+        max_length=300,
+        null=False,
+        verbose_name='쪽지 내용'
+    )
