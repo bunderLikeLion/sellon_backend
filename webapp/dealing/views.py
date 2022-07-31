b7db02808f53910169e5e8f9c413e7675a1900a8
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-01 01:44:09 +0900

    feat: completed_at이 오늘인 dealing count API

--- webapp/dealing/views.py
+++ webapp/dealing/views.py
@@ -1,3 +1,11 @@
-# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.utils import timezone
+from dealing.models import Dealing
 
-# Create your views here.
+
+class CountCompletedAPIView(APIView):
+
+    def get(self, request):
+        count = Dealing.objects.filter(completed_at__day=timezone.now().day).count()
+        return Response({'count': count})
