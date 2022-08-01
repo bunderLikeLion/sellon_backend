f8b677fc47099194417709f4ae874c7ddaf52c21
koomchang <leahpar0401@gmail.com>
2022-08-02 03:24:54 +0900

    fix: extra_kwargs 지움

--- webapp/product/serializers/product_group_serializer.py
+++ webapp/product/serializers/product_group_serializer.py
@@ -7,4 +7,3 @@ class ProductGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductGroup
         fields = ['id', 'user', 'auction', 'money', 'description']
-        extra_kwargs = {'auction_id': {'required': True}}
