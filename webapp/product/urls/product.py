18104a182505945241ff881da455c5cceacd8368
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-02 21:44:03 +0900

    fix: urls basename 관련 버그 수정

--- webapp/product/urls/product.py
+++ webapp/product/urls/product.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSet, ProductCategoryListAPIView
 
 router = DefaultRouter()
-router.register('', ProductViewSet)
+router.register('', ProductViewSet, basename='Product')
 
 urlpatterns = [
     path('category/', ProductCategoryListAPIView.as_view(), name='category-list')
--- webapp/product/urls/product_group.py
+++ webapp/product/urls/product_group.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from product.views import ProductGroupViewSet
 
 router = DefaultRouter()
-router.register('', ProductGroupViewSet)
+router.register('', ProductGroupViewSet, basename='ProductGroup')
 
 urlpatterns = []
 
