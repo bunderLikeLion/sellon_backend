3b9d19ddcdd7a4e9876430c65ee2f7d7688f9722
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-03 20:03:58 +0900

    docs: swagger용 comment 추가

--- webapp/auction/views/auction_view_set.py
+++ webapp/auction/views/auction_view_set.py
@@ -13,3 +13,38 @@ class AuctionViewSet(ModelViewSet):
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        """
+        경매장을 생성합니다.
+        """
+        return super().create(self, request, args, kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        경매장 상세 정보를 반환합니다.
+        """
+        return super().retrieve(self, request, args, kwargs)
+
+    def update(self, request, *args, **kwargs):
+        """
+        경매장 정보를 모두 수정합니다.
+        """
+        return super().update(self, request, args, kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        경매장 정보를 부분 수정합니다.
+        """
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        경매장을 삭제합니다.
+        """
+        return super().destroy(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        """
+        경매장 목록을 반환합니다.
+        """
+        return super().list(request, *args, **kwargs)
