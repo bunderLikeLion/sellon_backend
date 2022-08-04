55e70aa7a9655cb03450a0e633905c8d1b1df61c
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-04 22:19:22 +0900

    fix: per_page가 고정되어 나가는 이슈 수정 및 네이밍 수정

--- webapp/config/pagination.py
+++ webapp/config/pagination.py
@@ -2,17 +2,17 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
-class PageNumberPagination(PageNumberPagination):
-    page_size = 9
+class DefaultPagination(PageNumberPagination):
+    page_size = 10
     page_size_query_param = 'per_page'
     max_page_size = 30
     # 임시 설정
 
     def get_paginated_response(self, data):
         return Response({
-            'count': self.page.paginator.count,
-            'current_page': self.page.number,
-            'per_page': self.page_size,
+            'total_count': self.page.paginator.count,
             'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'per_page': self.get_page_size(self.request),
             'results': data,
         })
