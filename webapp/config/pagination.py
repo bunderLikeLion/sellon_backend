f1fa1c496808bc387335ecc4cc5ae60267290cb0
koomchang <leahpar0401@gmail.com>
2022-08-04 14:48:40 +0900

    fix: 프론트 요청으로 pagination 수정

--- webapp/config/pagination.py
+++ webapp/config/pagination.py
@@ -3,13 +3,14 @@ from rest_framework.response import Response
 
 
 class PageNumberPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 9
     page_size_query_param = 'per_page'
     max_page_size = 30
     # 임시 설정
 
     def get_paginated_response(self, data):
         return Response({
+            'count': self.page.paginator.count,
             'current_page': self.page.number,
             'per_page': self.page_size,
             'total_pages': self.page.paginator.num_pages,
