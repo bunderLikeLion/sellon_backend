7d2758329116f46bc72ce4d65ad6e2dbabf477ba
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-10 22:07:26 +0900

    chore: get_queryset 수정

--- webapp/dealing/views/dealing_evaluation_view.py
+++ webapp/dealing/views/dealing_evaluation_view.py
@@ -1,15 +1,14 @@
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.viewsets import GenericViewSet
 
+from config.viewsets import BaseViewSet
 from dealing.models import Dealing, DealingEvaluation
 from dealing.permissions import IsEvaluationEditable
 from dealing.serializers.dealing_evaluation_serializer import DealingEvaluationSerializer
 
 
-class DealingEvaluationView(CreateModelMixin, UpdateModelMixin, GenericViewSet, RetrieveAPIView):
-    queryset = DealingEvaluation.objects.all()
+class DealingEvaluationView(CreateModelMixin, UpdateModelMixin, RetrieveAPIView, BaseViewSet):
     serializer_class = DealingEvaluationSerializer
     permission_classes = [
         IsAuthenticatedOrReadOnly,
@@ -18,38 +17,25 @@ class DealingEvaluationView(CreateModelMixin, UpdateModelMixin, GenericViewSet, 
 
     lookup_field = 'dealing'
 
-    def get_object(self):
-        queryset = DealingEvaluation.objects.filter(dealing_id=self.kwargs['dealing'], evaluator=self.request.user)
-
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-
-        assert lookup_url_kwarg in self.kwargs, (self.__class__.__name__, lookup_url_kwarg)
-
-        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-        obj = get_object_or_404(queryset, **filter_kwargs)
-
-        self.check_object_permissions(self.request, obj)
-
-        return obj
+    def get_queryset(self):
+        return DealingEvaluation.objects.filter(evaluator=self.current_user)
 
     def perform_create(self, serializer):
-
-        dealing = Dealing.objects.select_related(
-            'product', 'product_group',
-            'product_group__user', 'product__user') \
-            .get(id=self.request.data['dealing_id'])
+        dealing = Dealing.objects.get(id=self.request.data['dealing_id'])
         owner = dealing.product.user
         client = dealing.product_group.user
 
-        if self.request.user != owner:
+        if self.current_user == owner:
             instance = serializer.save(
-                evaluator=self.request.user,
+                evaluator=self.current_user,
+                evaluated_user=client,
+            )
+        elif self.current_user == client:
+            instance = serializer.save(
+                evaluator=self.current_user,
                 evaluated_user=owner,
             )
         else:
-            instance = serializer.save(
-                evaluator=self.request.user,
-                evaluated_user=client,
-            )
+            return DealingEvaluation()
 
         return instance
