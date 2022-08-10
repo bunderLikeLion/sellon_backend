9d0bcf1bcb7d38b2381b7b0bc3beb7e782916162
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-10 21:03:44 +0900

    feat: dealing_evaluation_view get_object() override

--- webapp/dealing/views/dealing_evaluation_view.py
+++ webapp/dealing/views/dealing_evaluation_view.py
@@ -1,22 +1,36 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, get_object_or_404
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import GenericViewSet
 
 from dealing.models import Dealing, DealingEvaluation
 from dealing.permissions import IsEvaluationEditable
-from dealing.serializers.dealing_evaluation_serializer import UserEvaluationSerializer
+from dealing.serializers.dealing_evaluation_serializer import DealingEvaluationSerializer
 
 
 class DealingEvaluationView(CreateModelMixin, UpdateModelMixin, GenericViewSet, RetrieveAPIView):
     queryset = DealingEvaluation.objects.all()
-    serializer_class = UserEvaluationSerializer
+    serializer_class = DealingEvaluationSerializer
     permission_classes = [
         IsAuthenticatedOrReadOnly,
         IsEvaluationEditable,
     ]
 
     lookup_field = 'dealing'
+
+    def get_object(self):
+        queryset = DealingEvaluation.objects.filter(dealing_id=self.kwargs['dealing'], evaluator=self.request.user)
+
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (self.__class__.__name__, lookup_url_kwarg)
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
     def perform_create(self, serializer):
 
