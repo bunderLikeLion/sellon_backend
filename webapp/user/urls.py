75dbd2d17dc65fac7f45e991965c1eca5bba3a31
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-08-10 10:27:35 +0900

    feat: 유저의 거래 횟수 반환 API

--- webapp/user/urls.py
+++ webapp/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import DestroyUserAPIView, RetrieveUserAPIView
+from .views import DestroyUserAPIView, RetrieveUserAPIView, DealingsCountAPIView
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('dj_rest_auth.registration.urls')),
     path('user/destroy/', DestroyUserAPIView.as_view(), name='destroy'),
     path('user/<int:pk>', RetrieveUserAPIView.as_view(), name='retrieve'),
+    path('user/<int:pk>/dealings_count/', DealingsCountAPIView.as_view()),
 ]
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserAbstractSerializer
 from .models import User
+from dealing.models import Dealing
 
 
 class DestroyUserAPIView(generics.DestroyAPIView):
@@ -19,3 +21,14 @@ class DestroyUserAPIView(generics.DestroyAPIView):
 class RetrieveUserAPIView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserAbstractSerializer
+
+
+class DealingsCountAPIView(APIView):
+    def get(self, request, pk):
+        """
+        유저의 거래 횟수를 반환합니다.
+        """
+        product_count = Dealing.objects.filter(product__user=request.user).count()
+        product_group_count = Dealing.objects.filter(product_group__user=request.user).count()
+        count = product_count + product_group_count
+        return Response({'count': count})
