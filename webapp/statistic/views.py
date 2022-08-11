dc318e243894d5dc090cedab3049a3b4d63e415a
shinkeonkim <singun11@kookmin.ac.kr>
2022-08-11 20:10:44 +0900

    refactor: 코드 정리
    - swagger 문서에 같이 포함되도록 함.
    - urls 추가
    - 이루어진 dealings가 없을 경우 오류 처리 추가

--- webapp/statistic/views.py
+++ webapp/statistic/views.py
@@ -1,29 +1,74 @@
+from django.contrib.auth import get_user_model
 from django.db.models import Q, Count
-from rest_framework.views import APIView
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+from rest_framework import serializers
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
+
+from user.serializers.user_abstract_serializer import UserAbstractSerializer
 from dealing.models import Dealing
 from django.utils import timezone
 
 
-class MostProductDealingOfMonth(APIView):
+User = get_user_model()
+
+
+class UserDealingStatisticSerailizer(serializers.Serializer):
+    user = UserAbstractSerializer(read_only=True)
+    dealing_count = serializers.IntegerField()
+
+    class Meta:
+        fields = [
+            'user',
+            'count',
+        ]
+
+
+class MostProductDealingOfMonth(RetrieveAPIView):
+    serializer_class = UserDealingStatisticSerailizer
 
     def get(self, request):
         """
         한달간 직접 경매를 올려 거래한 수가 가장 많은 유저와 거래횟수를 반환합니다.
         """
-        most_product_dealing = Dealing.objects.filter(
+        dealing_product_statistics = Dealing.objects.filter(
             Q(completed_at__month=timezone.now().month) & Q(completed_at__isnull=False)
-        ).values('product__user').annotate(count=Count('product__user')).order_by('-count')[0]
-        return Response(most_product_dealing)
+        ).values('product__user').annotate(count=Count('product__user')).order_by('-count')
+
+        if len(dealing_product_statistics) == 0:
+            raise Http404()
+
+        most_dealing_product_statitic = dealing_product_statistics[0]
+
+        user = get_object_or_404(User, pk=most_dealing_product_statitic['product__user'])
+
+        return Response({
+            'user': UserAbstractSerializer(user).data,
+            'count': most_dealing_product_statitic['count']
+        })
 
 
-class MostProductGroupDealingOfMonth(APIView):
+class MostProductGroupDealingOfMonth(RetrieveAPIView):
+    serializer_class = UserDealingStatisticSerailizer
 
     def get(self, request):
         """
         한달간 가장 많이 거래한 유저와 거래횟수를 반환합니다.
         """
-        most_product_group_dealing = Dealing.objects.filter(
+        most_product_group_dealing_statitics = Dealing.objects.filter(
             Q(completed_at__month=timezone.now().month) & Q(completed_at__isnull=False)
-        ).values('product_group__user').annotate(count=Count('product_group__user')).order_by('-count')[0]
-        return Response(most_product_group_dealing)
+        ).values('product_group__user').annotate(count=Count('product_group__user')).order_by('-count')
+
+        if len(most_product_group_dealing_statitics) == 0:
+            raise Http404()
+
+        most_product_group_dealing_statitic = most_product_group_dealing_statitics[0]
+
+        user = get_object_or_404(User, pk=most_product_group_dealing_statitic['product_group__user'])
+
+        return Response({
+            'user': UserAbstractSerializer(user).data,
+            'count': most_product_group_dealing_statitic['count']
+        })
