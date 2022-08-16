331ab572b14e792e6398496f5e4b5f47d0f9f2bb
kokoa <singun11@kookmin.ac.kr>
2022-08-17 02:44:58 +0900

    Update webapp/dealing/views/dealing_history_view.py

--- webapp/dealing/views/dealing_history_view.py
+++ webapp/dealing/views/dealing_history_view.py
@@ -6,11 +6,11 @@ from dealing.serializers import DealingSerializer
 
 
 class DealingHistoryListAPIView(ListAPIView):
+    serializer_class = DealingSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
 
     def get_queryset(self):
         return Dealing.objects.all()\
             .filter(product_group__user=self.request.user, completed_at__isnull=False)
 
-    serializer_class = DealingSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = None
